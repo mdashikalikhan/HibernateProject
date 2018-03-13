b7b6322452bb181fa50a448f39520176dffaa17f
ashik.khan <ashik.khan@N1084.spftl.com>
2018-03-13 12:42:16 +0600

    Formatted

--- HirbernateProject/src/org/ashik/dto/Officers.java
+++ HirbernateProject/src/org/ashik/dto/Officers.java
@@ -14,42 +14,46 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="OFFICERS")
+@Table(name = "OFFICERS")
 public class Officers {
-	@Id @GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="OFFICER_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "OFFICER_ID")
 	private int id;
-	@Column(name="OFFICER_NAME")
+	@Column(name = "OFFICER_NAME")
 	private String name;
 	@OneToMany
-	@JoinTable(name="Offiers_Have_Vehicle", joinColumns=@JoinColumn(name="OFF_ID"),
-	inverseJoinColumns=@JoinColumn(name="VEH_ID"))
+	@JoinTable(name = "Offiers_Have_Vehicle", joinColumns = @JoinColumn(name = "OFF_ID"), inverseJoinColumns = @JoinColumn(name = "VEH_ID"))
 	private Collection<Vehicle> vehicle = new ArrayList<Vehicle>();
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Collection<Vehicle> getVehicle() {
 		return vehicle;
 	}
+
 	public void setVehicle(Collection<Vehicle> vehicle) {
 		this.vehicle = vehicle;
 	}
+
 	@Override
 	public String toString() {
 		return "Officers [id=" + id + ", name=" + name + ", vehicle=" + vehicle
 				+ "]";
 	}
-	
-	
 
 }
--- HirbernateProject/src/org/ashik/dto/Vehicle.java
+++ HirbernateProject/src/org/ashik/dto/Vehicle.java
@@ -11,33 +11,38 @@ import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 public class Vehicle {
-	@Id @GeneratedValue
-	@Column(name="VEHICLE_ID")
+	@Id
+	@GeneratedValue
+	@Column(name = "VEHICLE_ID")
 	private int vehicleId;
-	@Column(name="VEHICLE_NAME")
+	@Column(name = "VEHICLE_NAME")
 	private String vehicleName;
 	@ManyToOne
-	@NotFound(action=NotFoundAction.IGNORE)
+	@NotFound(action = NotFoundAction.IGNORE)
 	private Officers officer;
-	
+
 	public int getVehicleId() {
 		return vehicleId;
 	}
+
 	public void setVehicleId(int vehicleId) {
 		this.vehicleId = vehicleId;
 	}
+
 	public String getVehicleName() {
 		return vehicleName;
 	}
+
 	public void setVehicleName(String vediclName) {
 		this.vehicleName = vediclName;
 	}
+
 	public Officers getOfficer() {
 		return officer;
 	}
+
 	public void setOfficer(Officers officer) {
 		this.officer = officer;
 	}
-	
-	
+
 }
